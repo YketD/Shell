ea5393a2f6d22eec91c52db84f62f60342037d03
Marcel Germes <murdock.kaclan@gmail.com>
2018-03-19 02:00:29 +0100

    Redirects, pipelines

--- src/Pipeline.cpp
+++ src/Pipeline.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <unistd.h>
+#include <wait.h>
 #include "Pipeline.h"
 #include "SimpleCommand.h"
 
@@ -14,11 +16,60 @@ Pipeline::~Pipeline() {
  * Executes the commands on this pipeline.
  */
 void Pipeline::execute() {
-	std::cout << "FIXME: You should change Pipeline::execute()" << std::endl;
+	//std::cout << "FIXME: You should change Pipeline::execute()" << std::endl;
 
-	for( SimpleCommand *cmd : commands ) {
-		// FIXME: Probably need to set up some pipe here?
+    // Create pipes
+    auto pipeCount = static_cast<int>(commands.size() - 1);
+    int pipes[2*pipeCount];
+    for(unsigned int i = 0; i < pipeCount; i++)
+    {
+        if (pipe(pipes + i*2) < 0)
+        {
+            std::cout << "Piping failed" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+    }
 
-		cmd->execute();
-	}
+    int pid;
+    unsigned int idx = 0;
+    unsigned int j = 0;
+
+    for (SimpleCommand *cmd : commands) {
+        // FIXME: Probably need to set up some pipe here?
+
+        pid = fork();
+        if (pid == 0)
+        {
+            // If not last command
+            if (idx != (commands.size() - 1))
+            {
+                if (dup2(pipes[j + 1], STDOUT_FILENO) < 0)
+                {
+                    std::cout << "Dup2 out failed" << std::endl;
+                    exit(EXIT_FAILURE);
+                }
+            }
+
+            // If not first command
+            if (idx != 0)
+            {
+                if (dup2(pipes[j - 2], STDIN_FILENO) < 0)
+                {
+                    std::cout << "Dup2 in failed" << std::endl;
+                    exit(EXIT_FAILURE);
+                }
+            }
+
+            for(unsigned int i = 0; i < 2*pipeCount; i++)
+                close(pipes[i]);
+
+            cmd->execute();
+        }
+
+        idx++;
+        j += 2;
+    }
+
+    for(unsigned int i = 0; i < 2*pipeCount; i++)
+        close(pipes[i]);
 }
--- src/SimpleCommand.cpp
+++ src/SimpleCommand.cpp
@@ -4,29 +4,58 @@
 #include "SimpleCommand.h"
 
 void SimpleCommand::execute() {
-	std::cout << "FIXME: You should change SimpleCommand::execute()" << std::endl;
-	// FIXME: Your code here...
+	//std::cout << "FIXME: You should change SimpleCommand::execute()" << std::endl;
+
+    // Change directory
+    if (command.compare("cd") == 0)
+    {
+        if (arguments.empty())
+            chdir(getenv("HOME"));
+        else
+            chdir((char*) arguments[0].c_str());
+
+        exit(EXIT_SUCCESS);
+    }
+
+    // Redirects
+    int fd = -1;
+    int flags = O_CREAT;
+    for (const IORedirect &redirect : redirects)
+    {
+        if (redirect.getType() == IORedirect::Type::INPUT)
+            flags = flags | O_RDONLY;
+        else if (redirect.getType() == IORedirect::Type::OUTPUT)
+            flags = flags | O_WRONLY;
+        else if (redirect.getType() == IORedirect::Type::APPEND)
+            flags = flags | O_WRONLY | O_APPEND;
+
+        if ((fd = open(redirect.getNewFile().c_str(), flags)))
+        {
+            if (redirect.getType() == IORedirect::Type::INPUT)
+                close(STDIN_FILENO);
+            else if (redirect.getType() == IORedirect::Type::OUTPUT)
+                close(STDOUT_FILENO);
+            else if (redirect.getType() == IORedirect::Type::APPEND)
+                close(STDOUT_FILENO);
+
+            dup(fd);
+            close(fd);
+        }
+    }
 
     // create vector containing char to pass to exec call
-    std::vector<char*> argsChr = {};
-    argsChr.push_back(const_cast<char*>(command.c_str()));
-    for(auto const &arg : arguments)
+    std::vector<char *> argsChr = {};
+    argsChr.push_back(const_cast<char *>(command.c_str()));
+    for (auto const &arg : arguments)
     {
-        argsChr.emplace_back(const_cast<char*>(arg.c_str()));
+        argsChr.emplace_back(const_cast<char *>(arg.c_str()));
     }
     argsChr.emplace_back(nullptr);
 
-    // create fork of the process that will execute our command
-    int pid = fork();
-    if (pid == 0) {
-
-        if (redirects.size() > 0)
-        {
-            //open(redirects[0].getNewFile());
-        }
-
-        execvp(command.c_str(), argsChr.data());
+    int forkResult = execvp(command.c_str(), argsChr.data());
+    if (forkResult < 0)
+    {
+        std::cout << "Executing failed" << std::endl;
+        exit(EXIT_FAILURE);
     }
-
-    std::cout << "Redirects " << redirects.size() << std::endl;
 }
