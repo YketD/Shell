1e03cb82fc97ca2b93ba7a9d88653b31b863a345
Yke ten Donkelaar <yketd@hotmail.com>
2018-05-24 13:10:13 +0200

    Auto stash before merge of "master" and "origin/master"
    Comments added to pipeline.cpp

--- src/Pipeline.cpp
+++ src/Pipeline.cpp
@@ -16,16 +16,20 @@ Pipeline::~Pipeline() {
  * Executes the commands on this pipeline.
  */
 void Pipeline::execute() {
-
+    //check if a pipe is needed
     if (commands.size() == 1)
     {
         commands[0]->execute();
     }
     else
     {
-        // Create pipes
+        // check how many pipes are needed
         auto pipeCount = static_cast<int>(commands.size() - 1);
+
+        //create pipes array
         int pipes[2*pipeCount];
+
+        //creating pipes & writing to pipes array
         for(unsigned int i = 0; i < pipeCount; i++)
         {
             if (pipe(pipes + i*2) < 0)
@@ -34,16 +38,24 @@ void Pipeline::execute() {
                 exit(EXIT_FAILURE);
             }
         }
-
+        /* -- VARIABLE DECLARATION --
+            pid = system process id
+            idx = command id
+            j   = pipe id
+        */
         int pid;
         unsigned int idx = 0;
         unsigned int j = 0;
 
+        // Iterate through commands
         for (SimpleCommand *cmd : commands) {
 
+            // Fork process & save process id of current process
             pid = fork();
+
+            // in case of child process
             if (pid == 0)  {
-                // If not last command
+                // If not last command, write output to next command
                 if (idx != (commands.size() - 1))
                 {
                     if (dup2(pipes[j + 1], STDOUT_FILENO) < 0)
@@ -53,7 +65,7 @@ void Pipeline::execute() {
                     }
                 }
 
-                // If not first command
+                // If not first command, read input from last command
                 if (idx != 0)
                 {
                     if (dup2(pipes[j - 2], STDIN_FILENO) < 0)
@@ -63,17 +75,22 @@ void Pipeline::execute() {
                     }
                 }
 
+                //close all pipes in pipe array
                 for(unsigned int i = 0; i < 2*pipeCount; i++)
                     close(pipes[i]);
 
+                //execute the command
                 cmd->execute();
                 exit(EXIT_SUCCESS);
             }
-
+            /* -- UP ITERATORS --
+             command index incremented by 1 (next command)
+             pipe incremented by 2 (2 spaces in array : [in,out])
+             */
             idx++;
             j += 2;
         }
-
+        // close all pipes
         for(unsigned int i = 0; i < 2*pipeCount; i++)
             close(pipes[i]);
 
